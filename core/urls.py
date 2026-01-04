15e873fefee2749fece9d5b466077d643f77288f
govardhan <ccgovardhan4@gmail.com>
2026-01-04 15:20:48 +0530

    final

--- core/urls.py
+++ core/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +25,5 @@ urlpatterns = [
     path('api/facilities/', include('facilities.urls')),
     path('api/transport/', include('transport.urls')),
     path('api/analytics/', include('analytics.urls')),
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
